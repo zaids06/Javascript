6a611d45e582e416b0bb7960cebdbb2542cce0bd
Zaid Shah <99684512+zaids06@users.noreply.github.com>
2024-01-13 15:09:32 +0530

    Basics of Javascript practice

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,16 @@
-let ans="amazing"
-if (ans=="amazing") alert("this is amazing");
-console.log(10+20+39)
+// let ans="amazing"
+// if (ans=="amazing") alert("this is amazing");
+// console.log(10+20+39)
+
+//Triple equal to vs double equal to === vs ==
+
+// const num=Number(prompt("Enter the number"));
+// console.log(num)
+// if (num===10)
+//     console.log("The number is 10")
+//double equal to
+
+// const num=prompt("Enter the number");
+// console.log(num)
+// if (num==10)
+//     console.log("The number is 10")
